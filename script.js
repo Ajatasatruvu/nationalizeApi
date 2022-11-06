f475308033261843d14f9c5634a83feaa3c8387f
Harsha <pharshavardhan2@outlook.com>
2022-11-06 06:05:27 +0530

    added header using js

---
+++ script.js
@@ -0,0 +1,5 @@
+let header = document.createElement("header");
+let heading = document.createElement("h1");
+heading.textContent = "Predict person's country using name";
+header.append(heading);
+document.body.append(header);
