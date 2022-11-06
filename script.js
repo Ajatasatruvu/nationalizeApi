57aa2bdc5950d7d986032b15efb7572dfe8e26eb
Harsha <pharshavardhan2@outlook.com>
2022-11-06 06:07:32 +0530

    added form

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ let heading = document.createElement("h1");
 heading.textContent = "Predict person's country using name";
 header.append(heading);
 document.body.append(header);
+
+let main = document.createElement("main");
+let form = document.createElement("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  processSearch();
+});
+
+let searchInput = document.createElement("input");
+searchInput.type = "search";
+searchInput.placeholder = "Enter person's name";
+searchInput.required = true;
+form.append(searchInput);
+
+let searchButton = document.createElement("button");
+searchButton.textContent = "Search";
+form.append(searchButton);
+
+main.append(form);
+document.body.append(main);
